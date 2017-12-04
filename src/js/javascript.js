e16880ecd1ec69943f479c30d140e96dfdefd173
Tiago Porto <tiagoporto@ymail.com>
2017-12-03 23:10:56 -0200

    Update src files

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,5 +1,12 @@
 import domready from 'domready'
+import svgToInline from './svg-to-inline.js'
 
 domready(() => {
   // Executes after dow ready
+})
+
+svgToInline({
+  elementsClass: 'svg',
+  useTriggerClass: false,
+  preserveComments: false
 })
