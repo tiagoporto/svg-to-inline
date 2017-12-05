f0a9df6b099bd20ab9e8d76757643919052525f6
Tiago Porto <tiagoporto@ymail.com>
2017-12-05 20:25:19 -0200

    Add babel in mocha

--- src/js/svg-to-inline.test.js
+++ src/js/svg-to-inline.test.js
@@ -1,5 +1,7 @@
-describe('Name of the group test', () => {
-  it('should behave...', () => {
-    // Add assert
+import SvgToInline from './svg-to-inline.js'
+
+describe('Calling function', () => {
+  it('Should return ', () => {
+    true === true
   })
 })
