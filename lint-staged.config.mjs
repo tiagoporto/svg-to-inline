c75880b2bbfd20def233afeed608da4b6d7a3911
Tiago Porto <tiago@tiagoporto.com>
2024-12-18 12:07:59 -0300

    chore: enable lint in lint staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,4 +2,9 @@ export default {
   '*': 'prettier --check --ignore-unknown --write',
   '*.md': 'remark --frail',
   '*.{ts,tsx}': () => 'tsc --project tsconfig.json',
+  '*.{js,jsx,ts,tsx}': [
+    'eslint --ext js,jsx,ts,tsx --max-warnings 0',
+    // TODO: enable tests
+    // 'jest --bail --findRelatedTests --passWithNoTests',
+  ],
 }
